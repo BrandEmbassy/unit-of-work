84183e5eef7aa080969aa5213ebc0ac8c9558938
Michal Tichý <michal.tichy.84@gmail.com>
2023-04-18 09:19:04 +0200

    DE-42003: Assertion tool for Unit of work - with limited reasoning
    (Operation has no context on the interface)

---
+++ src/UnitOfWork/UnitOfWorkAssertions.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\UnitOfWork;
+
+use Nette\StaticClass;
+use PHPUnit\Framework\TestCase;
+use function count;
+use function get_class;
+use function implode;
+use function sprintf;
+
+/**
+ * @final
+ */
+class UnitOfWorkAssertions
+{
+    use StaticClass;
+
+
+    /**
+     * @param array<Operation> $expectedOperations
+     */
+    public static function assertOperationList(UnitOfWork $resultUnitOfWork, array $expectedOperations): void
+    {
+        $remainingOperations = [];
+        foreach ($resultUnitOfWork->getOperations() as $resultOperation) {
+            $remainingOperations = [];
+            foreach ($expectedOperations as $expectedOperation) {
+                /** @noinspection TypeUnsafeComparisonInspection */
+                if ($expectedOperation == $resultOperation) {
+                    continue;
+                }
+
+                $remainingOperations[] = $expectedOperation;
+            }
+
+            if (count($remainingOperations) === count($expectedOperations)) {
+                TestCase::fail(sprintf('Operation %s is not expected.', get_class($resultOperation)));
+            }
+
+            $expectedOperations = $remainingOperations;
+        }
+
+        if ($remainingOperations !== []) {
+            $operationClassNames = array_map(
+                static fn(Operation $operation): string => get_class($operation),
+                $remainingOperations,
+            );
+            TestCase::fail(
+                sprintf(
+                    'Some expected operations are missing: %s',
+                    implode(', ', $operationClassNames),
+                ),
+            );
+        }
+    }
+}
