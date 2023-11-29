52a6a084308b6a15d8de0a08aff3e5c6bd5be5f6
Milan Zelenka <milan@milanzelenka.com>
2023-11-29 10:14:53 +0100

    Revert "Removed UnitOfWorkReducer"
    This reverts commit 71ac3e4921a2b823c2ba87aae1aa2d9298fc2f1c.

---
+++ src/UnitOfWork/UnitOfWorkReducer.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\UnitOfWork;
+
+use function count;
+
+/**
+ * @final
+ */
+class UnitOfWorkReducer
+{
+    public function reduceFromBeginning(UnitOfWork $unitOfWorkToReduce, Operation $operationToReduceBy): UnitOfWork
+    {
+        $operations = $unitOfWorkToReduce->getOperations();
+
+        $operationsAfterReduction = [];
+        $index = 0;
+
+        if ($operationToReduceBy instanceof MergeableOperation) {
+            while (count($operations) > $index
+                && $operations[$index] instanceof MergeableOperation
+                && $operations[$index]->canBeMergedWith($operationToReduceBy)
+            ) {
+                $index++;
+            }
+        }
+
+        while (count($operations) > $index) {
+            $operationsAfterReduction[] = $operations[$index];
+            $index++;
+        }
+
+        return UnitOfWork::fromOperations($operationsAfterReduction);
+    }
+}
---
+++ src/UnitOfWork/UnitOfWorkReducerTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\UnitOfWork;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use function count;
+
+/**
+ * @final
+ */
+class UnitOfWorkReducerTest extends TestCase
+{
+    /**
+     * @dataProvider operationsToReduceProvider
+     *
+     * @param Operation[] $expectedOperationsAfterReduction
+     * @param Operation[] $operationsToReduce
+     */
+    public function testShouldReduceOperationsFromBeginning(
+        array $expectedOperationsAfterReduction,
+        array $operationsToReduce,
+        Operation $operationToReduceBy
+    ): void {
+        $reducer = new UnitOfWorkReducer();
+        $unitOfWorkToReduce = UnitOfWork::fromOperations($operationsToReduce);
+
+        $unitOfWorkAfterReduction = $reducer->reduceFromBeginning($unitOfWorkToReduce, $operationToReduceBy);
+        $actualOperationsAfterReduction = $unitOfWorkAfterReduction->getOperations();
+
+        $expectedOperationsCount = count($expectedOperationsAfterReduction);
+        Assert::assertCount($expectedOperationsCount, $actualOperationsAfterReduction);
+
+        for ($i = 0; $i < $expectedOperationsCount; $i++) {
+            Assert::assertSame($expectedOperationsAfterReduction[$i], $actualOperationsAfterReduction[$i]);
+        }
+    }
+
+
+    /**
+     * @return Operation[][]|Operation[][][]
+     */
+    public static function operationsToReduceProvider(): array
+    {
+        $mergeableOperation1 = new DefaultMergeableOperation('a');
+        $mergeableOperation2 = new DefaultMergeableOperation('b');
+
+        $notMergeableOperation1 = new NotMergeableOperation();
+        $notMergeableOperation2 = new NotMergeableOperation();
+
+        return [
+            [
+                [$notMergeableOperation1, $notMergeableOperation2],
+                [$mergeableOperation1, $notMergeableOperation1, $notMergeableOperation2],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [$notMergeableOperation1, $notMergeableOperation2],
+                [$mergeableOperation1, $mergeableOperation2, $notMergeableOperation1, $notMergeableOperation2],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [$notMergeableOperation1, $notMergeableOperation2],
+                [$notMergeableOperation1, $notMergeableOperation2],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [$notMergeableOperation1, $notMergeableOperation2, $mergeableOperation1],
+                [$notMergeableOperation1, $notMergeableOperation2, $mergeableOperation1],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [],
+                [$mergeableOperation1],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [],
+                [$mergeableOperation1, $mergeableOperation2],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [],
+                [],
+                new DefaultMergeableOperation('c'),
+            ],
+            [
+                [$notMergeableOperation1, $notMergeableOperation2, $mergeableOperation1],
+                [$notMergeableOperation1, $notMergeableOperation2, $mergeableOperation1],
+                new NotMergeableOperation(),
+            ],
+        ];
+    }
+}
